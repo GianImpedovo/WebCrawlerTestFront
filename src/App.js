79c7c3ee53b3ec5b74d04790ccbc87d161e9e401
Gianfranco Impedovo <impefranco98@gmail.com>
2024-08-01 20:33:31 -0300

    call to api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+
+let page = 1
+
 function App() {
+  const [data, setData] = useState([])
+
+  const getWordData = async () => {
+      const result = await axios.get(`http://localhost:8080/words?page=${page}`)
+      setData(result.data.docs);
+  }
+
+  useEffect(() => {
+    // loadFirstPage()
+    getWordData();
+
+  }, []);
+
+  const handleMas = async() => {
+
+      page += 1;
+      console.log(page);
+      const result = await axios.get(`http://localhost:8080/words?page=${page}`)
+
+      if(result.data.totalPages >= page){
+        setData(prevData => [...prevData, ...result.data.docs]);
+      }
+
+  }
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+      {
+        data.map( word => (
+            <p style={{fontSize: `${word.quantity * 20}px`, margin: '0 20px'}}>{word._id}</p>
+        ))
+      }
+      </div>
+      <div className='container'>
+      <button className='mas' onClick={handleMas}>Mas</button>
+      </div>
+      
     </div>
   );
 }
